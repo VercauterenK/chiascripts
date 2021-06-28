d7b922c10207b28390b95e3dff8d7cde62706929
VercauterenK <44348169+VercauterenK@users.noreply.github.com>
2021-06-28 06:10:35 +0200

    Update init.sh

--- harvest/init.sh
+++ harvest/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -O /etc/systemd/system/start-harvester.service https://raw.githubusercontent.com/iwex/chiascripts/main/harvest/start-harvester.service
+wget -O /etc/systemd/system/start-harvester-chaingreen.service https://raw.githubusercontent.com/iwex/chiascripts/main/harvest/start-harvester.service
 systemctl daemon-reload
 systemctl enable start-harvester
